0903be43a85fb71d6cacaf8bbbbe2fadf869a694
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2026-01-19 18:01:15 +0100

    udpated consts and 404.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,17 @@
 export const BLOG_COLLECTION_NAME = "blog";
 
+export const SITE: {
+  title: string;
+  description: string;
+  defaultLanguage: "en-us" | "ru-RU";
+  themes: Array<string>;
+} = {
+  title: "Digital Decay",
+  description: "Blog, notes and reports about development.",
+  defaultLanguage: "en-us",
+  themes: ["dark"],
+};
+
 export const SOCIALS = [
   {
     href: "https://github.com/takimoysha",
@@ -10,11 +22,12 @@ export const SOCIALS = [
     href: "https://linkedin.com/in/takimoysha",
     icon: "tabler:brand-linkedin",
     linkTitle: "LinkedIn",
-  }
+  },
 ];
 
 export default {
   BLOG_COLLECTION_NAME,
 
   SOCIALS,
+  SITE,
 };
