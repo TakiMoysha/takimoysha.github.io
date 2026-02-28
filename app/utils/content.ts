fcdb52516e7b41ace2d02854d836e9d3148b6628
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2026-02-28 11:13:14 +0100

    fix date parsing

--- app/utils/content.ts
+++ app/utils/content.ts
@@ -2,10 +2,17 @@ export function parseDate(numdate: number | string): Date | null {
   if (typeof numdate === 'number') numdate = String(numdate);
 
   if (typeof numdate === 'string') {
-    let date = `${numdate.slice(0, 4)}-${numdate.slice(4, 6)}-${numdate.slice(6, 8)}`;
+    // ISO format (2026-01-05T14:16)
+    if (numdate.includes('-') && numdate.includes('T')) {
+      return new Date(numdate);
+    }
 
-    if (numdate.length === 12) {
-      date += `T${numdate.slice(8, 10)}:${numdate.slice(10, 12)}`;
+    // Handle numeric format (20230101)
+    let date = '';
+    if (numdate.length >= 8) {
+      date = `${numdate.slice(0, 4)}-${numdate.slice(4, 6)}-${numdate.slice(6, 8)}`;
+    } else {
+      date = `00000000`;
     }
 
     return new Date(date);
